47a1b76d1bd7a6972cc081e78355c4dadf0975ef
DaleSwanson <crazysteve138@hotmail.com>
2013-08-30 20:32:46 -0400

    Initial commit

---
+++ reddit.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+#reddit parser by Dale Swanson Aug 30 2013
+#requires gnuplot
+
+use strict;
+use warnings;
+use autodie;
+use Cwd 'abs_path';
+abs_path($0) =~ m/(.*\/)/;
+my $dir = $1; #directory path of script
+#$|++; #autoflush disk buffer
+
+
+my $sub = 'http://www.reddit.com/r/dataisbeautiful/top/?sort=top&t=month'; #url for subreddit, blank to grab from command line
+if (!$sub) {$sub = $ARGV[0];}
+my $linxdump = 'reddit.html';
+my $debug=0;
+
+my $temp;
+my $reachedend = 0; #flag, set to 1 when we hit the last post
+my $url = $sub;
+my $postcount = 0;
+my @scores; #post scores
+my @hours; #post hours
+
+
+do
+{ 
+	#wget "http://www.reddit.com/r/dataisbeautiful/top/?sort=top&t=month" -O -  >reddit.txt
+	$temp = "wget \"$url\" -O -  >\"$linxdump\""; #get band top songs
+	#print "\n$temp\n";
+	if (!$debug) {system($temp);} #download page when not debug
+	if ($debug) {$linxdump = "dump.txt";} #if debug use saved page
+	
+	open my $ifile, '<', $linxdump;
+	
+	while (my $filecontents = <$ifile>) {
+		$postcount=0;
+		while ($filecontents =~ m/<div class="score likes">(\d+)<\/div>/g) {
+			$postcount++;
+			print "\nScore: $1";
+			$scores[$postcount] = $1;
+			
+		}
+		$postcount=0;
+		while ($filecontents =~ m/datetime=\"(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)-(\d+):(\d+)\"/g) {
+			$postcount++;
+			print "\tHour: $4";
+			$hours[$postcount] = $4;
+			
+		}
+		
+		if ($filecontents =~ m/view more:&#32;<a href=\"(\S+)\" rel=\"nofollow next\" >/)
+		{
+			$url = $1;
+		}
+		else
+		{
+			$reachedend=1;
+		}
+		print "\nURL: $url";
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	$reachedend=1;
+	
+} until ($reachedend);
+
+
+
+
+
+print "\nDone\n\n";
