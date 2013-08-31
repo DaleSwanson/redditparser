4fda69f3a590b8d57d0f234cb7c788bbd97ef456
DaleSwanson <crazysteve138@hotmail.com>
2013-08-30 21:31:10 -0400

    get UTC time insted of local time

--- reddit.pl
+++ reddit.pl
@@ -26,8 +26,9 @@ my $postcount = 0;
 my $totalposts = 0;
 my @scores; #post scores
 my @hours; #post hours
-my $dow;
-
+my @dows; #post day of week
+my %mon2num = qw(Jan 01 Feb 02 Mar 03 Apr 04 May 05 Jun 06 Jul 07 Aug 08 Sep 09 Oct 10 Nov 11 Dec 12);
+my %dow2num = qw(Sun 1 Mon 2 Tue 3 Wed 4 Thu 5 Fri 6 Sat 7);
 
 do
 {#grab each page of sub, get post data from each
@@ -47,15 +48,17 @@ do
 			$postcount++; #resets per page
 			$totalposts++; #a running count of all posts, does not reset
 			print "\nScore: $1";
-			$scores[$postcount] = $1;
+			push(@scores, $1);
 		}
-		$postcount=-1; #first date/time is community since, so ignore it
-		while ($filecontents =~ m/datetime=\"(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)-(\d+):(\d+)\"/g) {
+		#submitted&#32;<time title="1   2   3 4 :5 :6  7    UTC" datetime="2013-08-07T13:04:25-07:00">23 days</time>
+		#submitted&#32;<time title="Wed Aug 7 20:04:25 2013 UTC" datetime="2013-08-07T13:04:25-07:00">23 days</time>
+		while ($filecontents =~ m/submitted&#32;<time title=\"(\w{3}) (\w{3}) (\d{1,2}) (\d{2}):(\d{2}):(\d{2}) (\d{4}) UTC\" datetime=\"(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)-(\d+):(\d+)\"/g)
+		{
 			$postcount++;
-			#print "\tHour: $4";
-			$hours[$postcount] = $4;
-			$dow = Day_of_Week($1,$2,$3);
-			print "\nDate: $1-$2-$3 $4:$5:$6 \t$dow";
+			push(@hours, $4);
+			push(@dows, $dow2num{$1});
+			#$dow = Day_of_Week($1,$2,$3);
+			print "\nDate: $7-$2-$3 $4:$5:$6 \t$1";
 		}
 		
 		if ($filecontents =~ m/<a href=\"(\S+)\" rel=\"nofollow next\" >/)
@@ -68,7 +71,6 @@ do
 } until ($reachedend);
 
 #now we have all the data
-
 
 
 
